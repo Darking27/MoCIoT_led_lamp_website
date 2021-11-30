08ebed39967ae220f3e999ac13fcc043d9b8b192
Felix Rohn <felix.rohn@student.kit.edu>
2021-11-30 14:33:46 +0100

    added buttons for basic program selection

--- demo.js
+++ demo.js
@@ -15,3 +15,27 @@ function send() {
         socket.send("program:test");
     }
 }
+
+function red() {
+    if (socket !== null) {
+        socket.send("program:static red:255 green:0 blue:0");
+    }
+}
+
+function green() {
+    if (socket !== null) {
+        socket.send("program:static red:0 green:255 blue:0");
+    }
+}
+
+function blue() {
+    if (socket !== null) {
+        socket.send("program:static red:0 green:0 blue:255");
+    }
+}
+
+function rainbow() {
+    if (socket !== null) {
+        socket.send("program:rainbow");
+    }
+}
